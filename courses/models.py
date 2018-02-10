c54dbac9899185ca972502b89fc116ebeeab48c1
Yerbolat Amangeldi <yerbolat101@gmail.com>
2018-02-10 15:38:03 +0600

    [+] courses models without register

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Professor(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Exam(models.Model):
+
+
+    # 1 = Midterm1; 2 = Mid2; 3 = Quiz; 4 = Assignment; 5 = Final
+    exam_type = models.IntegerField(null=False)
+    course = models.ForeignKey(Course)
+    professor = models.ForeignKey(Professor)
+    created_at = models.DateTimeField(default=timezone.now())
+    # created_by = SOME USER
+
+    def __str__(self):
+        return "%s %s" %(self.course.name, self.exam_type)
+
+
+class Feedback(models.Model):
+    # grades = {
+    #     "A" : 1,
+    #     "A-" : 2,
+    #     ""
+    # }
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now())
+    course = models.ForeignKey(Course)
+    professor = models.ForeignKey(Professor, null=True)
+    # created_by = some user
+    # grade = "A" "A-" "B" "B+ etc"
+
+    def __str__(self):
+        return "%s : %s" % (str(self.created_at), self.course.name)
--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.utils import timezone
+from django.http import Http404, HttpResponse
 
-# Create your views here.
+def main_page(request):
+
+    return render(request, "main.html", context= {})
