1d200ab9c6964afe3a55a9b4155fb3f0c0b9879b
Yerbolat <sarah.hiyandao@gmail.com>
2018-03-15 17:14:01 +0600

    [+] change Password for internship and courses_list pages

--- internship/views.py
+++ internship/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import Internship
 from courses.models import Course
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib import messages
+from django.contrib.auth import login, logout, authenticate, update_session_auth_hash
 
 
 def main_page(request):
+    if request.method == 'POST' and request.POST['submit'] == 'Change Password':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('/')
+        else:
+            messages.error(request, 'Please correct the error below.')
     internship_list = Internship.objects.all()
     context = {
         "title": "Main Page",
@@ -11,4 +23,6 @@ def main_page(request):
     }
     course_list = Course.objects.all()
     context['course_list'] = course_list
+    form = PasswordChangeForm(request.user)
+    context['password_change_form'] = form
     return render(request, "internship/index.html", context)
